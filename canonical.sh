16fdb6b59607212380c9f5b8bc16bf7b9f335497
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-04-17 21:25:17 -0400

    Reworked to make sure the x3d file exists before running through diff

--- canonical.sh
+++ canonical.sh
@@ -7,7 +7,7 @@ javac -cp xmlunit/xmlunit-core/target/classes:xmlunit/xmlunit-legacy/target/clas
 cat /dev/null > javadiffresults.txt
 
 echo "@1 is when two strings are different lengths" | tee -a diffresults.txt
-echo "@2 is when two strings are unequal (quoting in word)" | tee -a diffresults.txt
+echo "@2 is when two strings are unequal (quoting in word, entities)" | tee -a diffresults.txt
 echo "@3 is when two strings are unequal (floats different length, platform issues" | tee -a diffresults.txt
 echo "@4 when the right file has a containerField" | tee -a diffresults.txt
 echo "@5 when the right file has a key" | tee -a diffresults.txt
@@ -17,16 +17,19 @@ echo "@8 is when the objects are unequal" | tee -a diffresults.txt
 
 for i in "$@"
 do
-	echo '#################################################' | tee -a diffresults.txt
 	export DIRNAME=`dirname "$i"`
  	export ORIGINAL=`basename "$i" -roundtrip.x3d`.x3d
  	export CANON=`basename "$i" -roundtrip.x3d`Canonical.x3d
 	export ROUNDTRIP=`basename "$i"`
 	export RTCANON=`basename "$i" .x3d`Canonical.x3d
-	
-	java -cp $CANONICALIZERDIST/log4j-1.2.15.jar:$CANONICALIZERDIST/xercesImpl.jar:$CANONICALIZERDIST/X3dC14n.jar org.web3d.x3d.tools.x3db.X3dCanonicalizer "$DIRNAME/$ORIGINAL"
-	java -cp $CANONICALIZERDIST/log4j-1.2.15.jar:$CANONICALIZERDIST/xercesImpl.jar:$CANONICALIZERDIST/X3dC14n.jar org.web3d.x3d.tools.x3db.X3dCanonicalizer "$DIRNAME/$ROUNDTRIP"
-	# echo diff "$DIRNAME/$CANON" "$DIRNAME/$RTCANON" 2>&1 | tee -a  javadiffresults.txt
-	# java -cp xmlunit/xmlunit-core/target/classes:xmlunit/xmlunit-legacy/target/classes:xmlunit/xmlunit-matchers/target/classes:. Compare "$DIRNAME/$CANON" "$DIRNAME/$RTCANON" 2>&1 | tee -a  javadiffresults.txt
-	node xmldiff.js "$DIRNAME/$CANON" "$DIRNAME/$RTCANON" 2>&1 | tee -a  diffresults.txt
+
+	if ls "$DIRNAME/$ORIGINAL"
+	then
+		echo '#################################################' | tee -a diffresults.txt
+		java -cp $CANONICALIZERDIST/log4j-1.2.15.jar:$CANONICALIZERDIST/xercesImpl.jar:$CANONICALIZERDIST/X3dC14n.jar org.web3d.x3d.tools.x3db.X3dCanonicalizer "$DIRNAME/$ORIGINAL"
+		java -cp $CANONICALIZERDIST/log4j-1.2.15.jar:$CANONICALIZERDIST/xercesImpl.jar:$CANONICALIZERDIST/X3dC14n.jar org.web3d.x3d.tools.x3db.X3dCanonicalizer "$DIRNAME/$ROUNDTRIP"
+		# echo diff "$DIRNAME/$CANON" "$DIRNAME/$RTCANON" 2>&1 | tee -a  javadiffresults.txt
+		# java -cp xmlunit/xmlunit-core/target/classes:xmlunit/xmlunit-legacy/target/classes:xmlunit/xmlunit-matchers/target/classes:. Compare "$DIRNAME/$CANON" "$DIRNAME/$RTCANON" 2>&1 | tee -a  javadiffresults.txt
+		node xmldiff.js "$DIRNAME/$CANON" "$DIRNAME/$RTCANON" 2>&1 | tee -a  diffresults.txt
+	fi
 done
